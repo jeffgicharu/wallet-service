b101b08c45b656610f6a40ed7897bdbbf0445cb8
jeffgicharu <jeff.kaharu@students.jkuat.ac.ke>
2025-07-02 17:25:30 +0300

    feat: Add registerUser mutation and AuthController

---
+++ src/main/java/com/digitalwallet/walletservice/controller/AuthController.java
@@ -0,0 +1,23 @@
+
+package com.digitalwallet.walletservice.controller;
+
+import com.digitalwallet.walletservice.model.AuthPayload;
+import com.digitalwallet.walletservice.service.AuthService;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @MutationMapping
+    public AuthPayload registerUser( @Argument String username, @Argument String phoneNumber, @Argument String pin) {
+        return authService.registerUser(username, phoneNumber, pin);
+    }
+}
